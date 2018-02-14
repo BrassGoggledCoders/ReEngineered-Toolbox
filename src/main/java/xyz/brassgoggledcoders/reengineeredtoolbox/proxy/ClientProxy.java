e69556ffff34f7f4625518a73072978f81306453
SkySom <skylar.som@gmail.com>
2018-02-14 00:11:10 -0500

    Remove unnecessary Imports

--- src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/proxy/ClientProxy.java
+++ src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/proxy/ClientProxy.java
@@ -1,12 +1,9 @@
 package xyz.brassgoggledcoders.reengineeredtoolbox.proxy;
 
 import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.reengineeredtoolbox.model.SocketModelLoader;
-import xyz.brassgoggledcoders.reengineeredtoolbox.render.TESRSocket;
-import xyz.brassgoggledcoders.reengineeredtoolbox.tileentity.TileEntitySocket;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy implements IProxy {
