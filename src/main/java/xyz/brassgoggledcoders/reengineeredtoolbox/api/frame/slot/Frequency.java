e970790c12ffad26e74dc954e03a48b7ade9163f
Skylar <skylar.som@gmail.com>
2023-06-19 00:08:53 -0400

    Frequence.getColor is now DyeColor

--- src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/api/frame/slot/Frequency.java
+++ src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/api/frame/slot/Frequency.java
@@ -1,39 +1,39 @@
 package xyz.brassgoggledcoders.reengineeredtoolbox.api.frame.slot;
 
-import net.minecraft.ChatFormatting;
+import net.minecraft.world.item.DyeColor;
 
 import java.util.Optional;
 
 public enum Frequency {
-    BLACK(ChatFormatting.BLACK),
-    DARK_BLUE(ChatFormatting.DARK_BLUE),
-    DARK_GREEN(ChatFormatting.DARK_GREEN),
-    DARK_AQUA(ChatFormatting.DARK_AQUA),
-    DARK_RED(ChatFormatting.DARK_RED),
-    DARK_PURPLE(ChatFormatting.DARK_PURPLE),
-    GOLD(ChatFormatting.GOLD),
-    GRAY(ChatFormatting.GRAY),
-    DARK_GRAY(ChatFormatting.DARK_GRAY),
-    BLUE(ChatFormatting.BLUE),
-    GREEN(ChatFormatting.GREEN),
-    AQUA(ChatFormatting.AQUA),
-    RED(ChatFormatting.RED),
-    LIGHT_PURPLE(ChatFormatting.LIGHT_PURPLE),
-    YELLOW(ChatFormatting.YELLOW),
-    WHITE(ChatFormatting.WHITE);
+    WHITE(DyeColor.WHITE),
+    ORANGE(DyeColor.ORANGE),
+    MAGENTA(DyeColor.MAGENTA),
+    LIGHT_BLUE(DyeColor.LIGHT_BLUE),
+    YELLOW(DyeColor.YELLOW),
+    LIME(DyeColor.LIME),
+    PINK(DyeColor.PINK),
+    GRAY(DyeColor.GRAY),
+    LIGHT_GRAY(DyeColor.LIGHT_GRAY),
+    CYAN(DyeColor.CYAN),
+    PURPLE(DyeColor.PURPLE),
+    BLUE(DyeColor.BLUE),
+    BROWN(DyeColor.BROWN),
+    GREEN(DyeColor.GREEN),
+    RED(DyeColor.RED),
+    BLACK(DyeColor.BLACK);
 
-    private final ChatFormatting color;
+    private final DyeColor color;
 
-    Frequency(ChatFormatting color) {
+    Frequency(DyeColor color) {
         this.color = color;
     }
 
-    public ChatFormatting getColor() {
+    public DyeColor getColor() {
         return color;
     }
 
     public static Optional<Frequency> getByName(String name) {
-        for (Frequency frequency: Frequency.values()) {
+        for (Frequency frequency : Frequency.values()) {
             if (frequency.name().equalsIgnoreCase(name)) {
                 return Optional.of(frequency);
             }
