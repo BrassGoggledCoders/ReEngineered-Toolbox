2d659951138c2e6d2f918ea37ecd6e87409cd24e
Skylar <skylar.som@gmail.com>
2023-06-19 13:17:23 -0400

    Slots now show Text

--- src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/api/frame/slot/FrameSlotView.java
+++ src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/api/frame/slot/FrameSlotView.java
@@ -1,5 +1,8 @@
 package xyz.brassgoggledcoders.reengineeredtoolbox.api.frame.slot;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.Vec3;
+
 public record FrameSlotView(
         FrameSlot frameSlot,
         float xPos,
@@ -7,4 +10,36 @@ public record FrameSlotView(
         float height,
         float width
 ) {
+
+    public boolean isInside(Vec3 look, Direction direction) {
+        double xLook;
+        double yLook;
+
+        if (direction.getAxis().isHorizontal()) {
+            yLook = look.y() - (int) look.y();
+        } else {
+            yLook = direction.getAxisDirection() == Direction.AxisDirection.POSITIVE ? -look.z() : look.z();
+        }
+
+        yLook = Math.abs(yLook);
+        yLook = yLook - (int) yLook;
+        yLook *= 16D;
+
+        xLook = switch (direction.getAxis()) {
+            case X -> look.z();
+            case Y -> direction.getAxisDirection() == Direction.AxisDirection.POSITIVE ? look.x() : -look.x();
+            case Z -> look.x();
+        };
+
+        xLook = Math.abs(xLook);
+        xLook = xLook - (int) xLook;
+        xLook *= 16D;
+        //WHY EAST AND WEST?
+        if (direction == Direction.EAST || direction == Direction.NORTH || direction == Direction.DOWN) {
+            xLook = 16 - xLook;
+        }
+
+        return xLook > this.xPos() && xLook < this.xPos() + this.width() &&
+                yLook > this.yPos() && yLook < this.yPos() + this.height();
+    }
 }
---
+++ src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/event/ForgeClientEventHandler.java
@@ -0,0 +1,69 @@
+package xyz.brassgoggledcoders.reengineeredtoolbox.event;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import xyz.brassgoggledcoders.reengineeredtoolbox.ReEngineeredToolbox;
+import xyz.brassgoggledcoders.reengineeredtoolbox.api.frame.slot.FrameSlotView;
+import xyz.brassgoggledcoders.reengineeredtoolbox.api.panelentity.PanelEntity;
+import xyz.brassgoggledcoders.reengineeredtoolbox.blockentity.FrameBlockEntity;
+import xyz.brassgoggledcoders.reengineeredtoolbox.content.ReEngineeredItemTags;
+
+import java.util.List;
+
+@EventBusSubscriber(modid = ReEngineeredToolbox.ID, value = Dist.CLIENT, bus = Bus.FORGE)
+public class ForgeClientEventHandler {
+
+    @SubscribeEvent
+    public static void addFrameSlotText(RenderGuiOverlayEvent.Post event) {
+        if (Minecraft.getInstance().hitResult instanceof BlockHitResult blockHitResult) {
+            Player player = Minecraft.getInstance().player;
+            if (player != null && player.getMainHandItem().is(ReEngineeredItemTags.CAN_ALTER_FRAME_SLOT)) {
+                if (player.getLevel().getBlockEntity(blockHitResult.getBlockPos()) instanceof FrameBlockEntity frameBlockEntity) {
+                    PanelEntity panelEntity = frameBlockEntity.getPanelEntity(blockHitResult.getDirection());
+                    if (panelEntity != null) {
+                        int scaledHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+                        int scaledWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+
+                        List<FrameSlotView> viewList = panelEntity.getFrameSlotViews();
+                        for (FrameSlotView frameSlotView : viewList) {
+                            if (frameSlotView.isInside(blockHitResult.getLocation(), blockHitResult.getDirection())) {
+                                drawBlockOverlayText(event.getPoseStack(), new Component[]{frameSlotView.frameSlot().getName()}, scaledWidth, scaledHeight);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //From: https://github.com/BluSunrize/ImmersiveEngineering/blob/1.19.2/src/main/java/blusunrize/immersiveengineering/client/BlockOverlayUtils.java#L51
+    public static void drawBlockOverlayText(PoseStack transform, Component[] text, int scaledWidth, int scaledHeight) {
+        if (text != null && text.length > 0) {
+            int i = 0;
+            MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+            Font font = Minecraft.getInstance().font;
+            for (Component s : text) {
+                if (s != null) {
+                    font.drawInBatch(
+                            Language.getInstance().getVisualOrder(s),
+                            scaledWidth / 2F + 8, scaledHeight / 2F + 8 + (i++) * font.lineHeight, 0xffffffff, true,
+                            transform.last().pose(), buffer, false, 0, 0xf000f0
+                    );
+                }
+            }
+
+            buffer.endBatch();
+        }
+    }
+}
