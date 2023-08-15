0166135311de784b21c467aac3b90be5bbff6555
Skylar <skylar.som@gmail.com>
2023-08-15 00:03:12 -0400

    Switch RecipeCache to allow for recents

--- src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/recipe/RecipeCache.java
+++ src/main/java/xyz/brassgoggledcoders/reengineeredtoolbox/recipe/RecipeCache.java
@@ -10,16 +10,27 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.common.util.INBTSerializable;
 import xyz.brassgoggledcoders.reengineeredtoolbox.util.functional.Option;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeCache<T extends Recipe<U>, U extends Container> implements INBTSerializable<StringTag> {
     private final RecipeType<T> recipeType;
 
     private ResourceLocation lastRecipeId;
     private T lastRecipe;
+    private final List<T> savedChecks;
+    private final int maxSavedChecks;
 
     private boolean tryGet = true;
 
     public RecipeCache(RecipeType<T> recipeType) {
+        this(recipeType, 0);
+    }
+
+    public RecipeCache(RecipeType<T> recipeType, int savedChecks) {
         this.recipeType = recipeType;
+        this.savedChecks = new ArrayList<>(savedChecks);
+        this.maxSavedChecks = savedChecks;
     }
 
     public void reset() {
@@ -35,14 +46,33 @@ public class RecipeCache<T extends Recipe<U>, U extends Container> implements IN
         }
 
         if (lastRecipe != null && !lastRecipe.matches(container, level)) {
+            if (maxSavedChecks > 0 && !savedChecks.contains(lastRecipe)) {
+                if (savedChecks.size() == maxSavedChecks) {
+                    savedChecks.remove(0);
+                }
+                savedChecks.add(lastRecipe);
+            }
             lastRecipe = null;
             tryGet = true;
         }
 
         if (lastRecipe == null && tryGet) {
-            this.lastRecipe = level.getRecipeManager()
-                    .getRecipeFor(recipeType, container, level)
-                    .orElse(null);
+            if (maxSavedChecks > 0) {
+                for (T pastRecipe : savedChecks) {
+                    if (pastRecipe.matches(container, level)) {
+                        this.lastRecipe = pastRecipe;
+                    }
+                    if (this.lastRecipe != null) {
+                        this.savedChecks.remove(this.lastRecipe);
+                    }
+                }
+            }
+            if (this.lastRecipe == null) {
+                this.lastRecipe = level.getRecipeManager()
+                        .getRecipeFor(recipeType, container, level)
+                        .orElse(null);
+            }
+
             this.tryGet = false;
         }
 
